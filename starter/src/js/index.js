38865b226ab3007673fae20197afad9bcb6b36df
ShotaTogawa <s.togawa0112@gmail.com>
2020-02-26 20:40:51 -0800

    added loader and pagination

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchViews";
-import { elements } from "./views/base";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /** Global state of the app
  * - Search object
@@ -20,16 +20,27 @@ const controlSearch = async () => {
     // 3) Prepare UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4) Search for recipe
     await state.search.getResults();
 
     // 5) render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
 
-document.querySelector(".search").addEventListener("submit", e => {
+elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
+});
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
--- starter/src/js/views/searchViews.js
+++ starter/src/js/views/searchViews.js
@@ -7,6 +7,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
   elements.searchResList.innerHTML = "";
+  elements.searchResPages.innerHTML = "";
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
@@ -44,6 +45,46 @@ const renderRecipe = recipe => {
   elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = recipes => {
-  recipes.forEach(recipe => renderRecipe(recipe));
+const createButton = (page, type) => `
+  <button class="btn-inline results__btn--${type}" data-goto=${
+  type === "prev" ? page - 1 : page + 1
+}>
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-${
+          type === "prev" ? "left" : "right"
+        }"></use>
+    </svg>
+    <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
+  </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+  console.log(pages);
+  let button;
+  if (page === 1 && pages > 1) {
+    // button to go to next page
+    button = createButton(page, "next");
+  } else if (page < pages) {
+    // both button
+    button = `
+      ${createButton(page, "prev")}
+      ${createButton(page, "next")}
+    `;
+  } else if (page === pages && pages > 1) {
+    // button to go to next page
+    button = createButton(page, "prev");
+  }
+
+  elements.searchResPages.insertAdjacentHTML("afterbegin", button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
+  recipes.slice(start, end).forEach(recipe => renderRecipe(recipe));
+
+  // render pagination button
+  renderButtons(page, recipes.length, resPerPage);
 };
